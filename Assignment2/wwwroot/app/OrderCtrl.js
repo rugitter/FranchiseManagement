ddfeab7989237e2185de6dc5e7f609ed5d111b7b
Casey Yan“ <s3679535@student.rmit.edu.au>
2018-05-21 18:21:05 +1000

    order history

--- Assignment2/wwwroot/app/OrderCtrl.js
+++ Assignment2/wwwroot/app/OrderCtrl.js
@@ -6,27 +6,27 @@ angular.module('myApp', []).controller('OrderCtrl', function ($scope) {
     $scope.OrderDate = '';
 
     $scope.orders = [
-        { id: 1, OrderID: '1', OrderDate: "20/5/18" },
-        { id: 2, OrderID: '2', OrderDate: "20/5/18" },
-        { id: 3, OrderID: '3', OrderDate: "20/5/18" },
-        { id: 4, OrderID: '4', OrderDate: "20/5/18" },
-        { id: 5, OrderID: '5', OrderDate: "20/5/18" },
-        { id: 6, OrderID: '6', OrderDate: "20/5/18" }
+        { OrderID: 1, OrderID: '1', OrderDate: "20/5/18" },
+        { OrderID: 2, OrderID: '2', OrderDate: "20/5/18" },
+        { OrderID: 3, OrderID: '3', OrderDate: "20/5/18" },
+        { OrderID: 4, OrderID: '4', OrderDate: "20/5/18" },
+        { OrderID: 5, OrderID: '5', OrderDate: "20/5/18" },
+        { OrderID: 6, OrderID: '6', OrderDate: "20/5/18" }
     ];
     $scope.view = true;
     $scope.error = false;
     $scope.incomplete = false;
 
-    $scope.viewOrder = function (id) {
-        if (id == 'new') {
+    $scope.viewOrder = function (OrderID) {
+        if (OrderID == 'new') {
             $scope.view = true;
             $scope.incomplete = true;
             $scope.OrderID = '';
             $scope.OrderDate = '';
         } else {
             //$scope.edit = false;
-            $scope.OrderID = $scope.orders[id - 1].OrderID;
-            $scope.OrderDate = $scope.orders[id - 1].OrderDate;
+            $scope.OrderID = $scope.orders[OrderID - 1].OrderID;
+            $scope.OrderDate = $scope.orders[OrderID - 1].OrderDate;
         }
     };
 
